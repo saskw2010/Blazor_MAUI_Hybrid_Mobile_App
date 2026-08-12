1295902d932f04b16e0c53a88b9c21c0cf7b2c6e
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 01:51:02 +0300

    Publish standalone mobile project file: BackgroundCss.cs

---
+++ Taskmote.SharedUI/Extensions/BackgroundCss.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace Taskmote.SharedUI.Extensions
+{
+    public enum BGSize
+    {
+        auto,
+        cover,
+        contain,
+    }
+
+    public interface ICssBuilder
+    {
+        public void Add(string part);
+        public string ToCss();
+    }
+
+    public class Css : ICssBuilder
+    {
+        public static ICssBuilder Build() => new Css();
+
+        StringBuilder _builder = new StringBuilder();
+        public void Add(string part) => _builder.Append(part + ";");
+        public string ToCss() => _builder.ToString();
+    }
+
+    public static class CssBackground
+    {
+        const string BACKGROUNDCOLOR = "background-color: {0}";
+        const string BACKGROUNDSIZE = "background-size: {0}";
+        
+
+        public static ICssBuilder BGColor(this ICssBuilder cssBuilder, string value)
+        {
+            cssBuilder.Add(string.Format(BACKGROUNDCOLOR, value));
+            return cssBuilder;
+        }
+
+        public static ICssBuilder BGSize(this ICssBuilder cssBuilder, BGSize value)
+        {
+            cssBuilder.Add(string.Format(BACKGROUNDCOLOR, value.ToString()));
+            return cssBuilder;
+        }
+    }
+
+}
+
