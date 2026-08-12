04dd01a29bab458e78682485bb19bbf1417ef399
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 01:51:43 +0300

    Publish standalone mobile project file: Program.cs

---
+++ Taskmote.Wasm/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Taskmote.SharedUI.Services;
+using Taskmote.Wasm;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+Console.WriteLine($"***** Baseaddress is {builder.HostEnvironment.BaseAddress}");
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+await builder.Build().RunAsync();
+
